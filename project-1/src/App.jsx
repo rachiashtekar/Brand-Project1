1114225761f9267417840296204fc487000c134f
rachi <rachiashtekar345@gmail.com>
2024-04-04 15:04:38 +0530

    .

---
+++ project-1/src/App.jsx
@@ -0,0 +1,19 @@
+
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navigation from './components/Navigation'
+
+import HeroSection from './components/Hero'
+
+function App() {
+
+
+  return (
+  <div>
+   <Navigation/>
+   <HeroSection/>
+  </div>
+  )
+}
+
+export default App
